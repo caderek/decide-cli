fffb8827aea28b86838760cbb5d8d081a5e05afb
Maciej Caderek <maciej@brainhub.pl>
2017-02-19 02:10:06 +0100

    Removed login panel

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import LoginPanel from '../components/LoginPanel'
 import IssueList from '../containers/IssueList'
 
 const App = () => (
   <div>
-    {window.localStorage.getItem('jwt') === undefined ? <LoginPanel /> : null}
     <IssueList />
   </div>
 )
